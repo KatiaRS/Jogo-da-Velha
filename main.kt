6831af624527bfbc9d24b81ec09a90f2ace1eb65
KatiaRS <katiasantana2012@hotmail.com>
2023-05-08 16:32:37 -0300

    Implementando com o enum

--- main.kt
+++ main.kt
@@ -1,35 +1,15 @@
-/**
-Jogo da velha é um jogo simples para dois jogadores que utilizam os símbolos X e O para
-marcar suas jogadas em uma matriz 3x3. O jogador que colocar o seu símbolo 3 vezes seguidas na horizontal,
-na vertical ou na diagonal vence. Seu objetivo é implementar um jogo da velha utilizando a liguagem de
-programação Kotlin e conceitos de orientação a objeto.
+enum class Jogador(var simbolo: String) {
+    X("X"),
+    O("O"),
+    VAZIO(" ");
 
-Requisitos funcionais do sistema:
-Deve ser possível nomear os dois jogadores, o nome do jogador deve ser exibido antes de cada jogada;
-
-O jogo deve ser jogado via console da aplicação, onde a cada jogada o jogador escolhe em qual posição
-deseja marcar seu respectivo símbolo no tabuleiro;
-
-O jogo é finalizado quando algum jogador colocar 3 símbolos seguidos na horizontal, na vertical ou na
-diagonal. Pode ocorrer também um empate, que acontece quando o tabuleiro é preenchido totalmente e nenhum
-dos dois jogadores conseguiu ter 3 símbolos consecutivos no tabuleiro;
-
-O tabuleiro deve ser impresso no console em seu estado atual após cada jogada;
-
-Deve ser mantido o número de vitórias, derrotas e empates de cada jogador;
-
-Deve ser possível recomeçar o jogo com novos jogadores, limpando o histórico de partidas dos antigos
-jogadores;
-
-O projeto deve ser disponibilizado no github, deve ser implementado em uma branch separada da master,
-com commits atômicos e com mensagens sucintas descrevendo o que foi feito em cada commit. Ao fim do projeto
-deve ser aberta uma pull request para a master para que os outros desenvolvedores do time possam revisar o
-projeto;
- */
+    override fun toString(): String {
+        return simbolo
+    }
+}
 
 class JogoDaVelha {
-    //Método Imprimir Tabuleiro
-    fun imprimirTabuleiro(tabuleiro: Array<CharArray>) {
+    fun imprimirTabuleiro(tabuleiro: Array<Array<Jogador>>) {
 
         for (linha in 0 until 3) {
             for (coluna in 0 until 3) {
@@ -45,12 +25,11 @@ class JogoDaVelha {
         }
     }
 
-    //Metodo Jogar
-    fun jogar(linha: Int, coluna: Int, tabuleiro: Array<CharArray>): Boolean {
+    fun validaJogada(linha: Int, coluna: Int, tabuleiro: Array<Array<Jogador>>): Boolean {
         if (linha < 0 || linha >= 3 || coluna < 0 || coluna >= 3) {      //validando a posição se existe ou não
             return false
         }
-        if (tabuleiro[linha][coluna] != ' ') {   //validar se está preenchido ou não
+        if (tabuleiro[linha][coluna] != Jogador.VAZIO) {   //validar se está preenchido ou não
             return false
         }
 
@@ -58,10 +37,10 @@ class JogoDaVelha {
     }
 
     //verifica se algum jogador ganhou nas linhas
-    fun verificarGanhadorLinha(tabuleiro: Array<CharArray>): Boolean {
+    fun verificaGanhadorLinha(tabuleiro: Array<Array<Jogador>>): Boolean {
         for (linha in 0 until 3) {
             if (tabuleiro[linha][0] == tabuleiro[linha][1] && tabuleiro[linha][1] == tabuleiro[linha][2]
-                && tabuleiro[linha][0] != ' '
+                && tabuleiro[linha][0] != Jogador.VAZIO
             ) {
                 return true
             }
@@ -69,10 +48,10 @@ class JogoDaVelha {
         return false
     }
 
-    fun verificarGanhadorColuna(tabuleiro: Array<CharArray>): Boolean {
+    fun verificaGanhadorColuna(tabuleiro: Array<Array<Jogador>>): Boolean {
         for (coluna in 0 until 3) {
             if (tabuleiro[0][coluna] == tabuleiro[1][coluna] && tabuleiro[1][coluna] == tabuleiro[2][coluna]
-                && tabuleiro[0][coluna] != ' '
+                && tabuleiro[0][coluna] != Jogador.VAZIO
             ) {
                 return true
             }
@@ -80,33 +59,47 @@ class JogoDaVelha {
         return false
     }
 
-    fun verificarGanhadorDiagonal(tabuleiro: Array<CharArray>): Boolean {
+    fun verificaGanhadorDiagonal(tabuleiro: Array<Array<Jogador>>): Boolean {
         if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]
-            && tabuleiro[0][0] != ' '
+            && tabuleiro[0][0] != Jogador.VAZIO
         ) {
             return true
         }
         if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]
-            && tabuleiro[0][2] != ' '
+            && tabuleiro[0][2] != Jogador.VAZIO
         ) {
             return true
         }
         return false
     }
 
-    fun verificarVencedores(tabuleiro: Array<CharArray>): Boolean {
-        return verificarGanhadorColuna(tabuleiro) || verificarGanhadorDiagonal(tabuleiro) ||
-                verificarGanhadorLinha(tabuleiro)
+    fun existeVencedor(tabuleiro: Array<Array<Jogador>>): Boolean {
+        return verificaGanhadorColuna(tabuleiro) || verificaGanhadorDiagonal(tabuleiro) ||
+                verificaGanhadorLinha(tabuleiro)
     }
 
+//    fun reiniciarJogo(tabuleiro: Array<Array<Char?>>, jogadorAtual,) {
+//        for (linha in 0 until 3) {
+//            for (coluna in 0 until 3){
+//                imprimirTabuleiro(tabuleiro)
+//            }
+//        }
+//        jogadorAtual = when (jogadorAtual){
+//            Jogador.X == Jogador.O
+//            Jogador.O == Jogador.X
+//        }
+//        println("O jogo foi reiniciado!")
+//        println("Iniciando pelo Jogador ${jogadorAtual.simbolo}")
+//    }
 
-    fun novoJogo(tabuleiro: Array<CharArray>) {
+
+    fun novoJogo(tabuleiro: Array<Array<Jogador>>) {
         println("Deseja uma nova jogada?")
         var resposta = readln()
         if (resposta.equals("Sim", true)) {
+
             println("Iniciando o jogo com novos jogadores")
-            val tabuleiro = Array(3) { CharArray(3) { ' ' } }
-            imprimirTabuleiro(tabuleiro)
+            val tabuleiro = Array(3) { Array<Jogador>(3) { Jogador.VAZIO } }
             main()
         }
     }
@@ -134,15 +127,20 @@ fun main() {
     println("Iniciando o jogo com os jogadores $jogador1 e $jogador2!!")
     println("Analise o tabuleiro abaixo:")
 
-    //Imprimindo Tabuleiro
-    val tabuleiro = Array(3) { CharArray(3) { ' ' } }
+
+    //val tabuleiro = Array(3, { Array<Jogador>(3, {Jogador.VAZIO})})
+    val tabuleiro = arrayOf(
+        arrayOf(Jogador.VAZIO, Jogador.VAZIO, Jogador.VAZIO),
+        arrayOf(Jogador.VAZIO, Jogador.VAZIO, Jogador.VAZIO),
+        arrayOf(Jogador.VAZIO, Jogador.VAZIO, Jogador.VAZIO)
+    )
     jogo.imprimirTabuleiro(tabuleiro)
 
     //Primeira jogada
 
     var jogadorAtual = jogador1
 
-    while (jogada < 9) {
+    while (jogada < 9 && !jogo.existeVencedor(tabuleiro)) {
 
         println("$jogadorAtual sua vez de jogar")
         println("Digite a posição da linha de 0 a 2")
@@ -153,7 +151,7 @@ fun main() {
 
         //validar a entrada do jogador
         //enquanto a posição estiver errada, pedir para o jogador inserir a entrada novamente.
-        while (jogo.jogar(linha, coluna, tabuleiro) == false) {
+        while (jogo.validaJogada(linha, coluna, tabuleiro) == false) {
             println("Opção Inválida, tente novamente.")
             println("Digite a posição da linha de 0 a 2")
             linha = readLine()!!.toInt()
@@ -161,16 +159,15 @@ fun main() {
             coluna = readLine()!!.toInt()
         }
 
-        tabuleiro[linha][coluna] = if (jogadorAtual == jogador1) 'X' else 'O'
+          tabuleiro[linha][coluna] = if (jogadorAtual == jogador1) Jogador.X else Jogador.O
 
         jogo.imprimirTabuleiro(tabuleiro)
 
 
         //verificar de existe ganhador ou não, se houver ganhador parar o jogo.
 
-        if (jogo.verificarVencedores(tabuleiro) == true) {
+        if (jogo.existeVencedor(tabuleiro) == true) {
             //vai parar o jogo
-            jogada = 9
             println("$jogadorAtual você ganhou o jogo!!")
             if (jogadorAtual == jogador1) {
                 vitoriaJogador1++
@@ -192,7 +189,7 @@ fun main() {
     }
     //qual a resposta do jogo, houve ganhador ou empate?
     //se o resultado da função verificar Vencedores for falso,ele me mostra um empate.
-    if (jogo.verificarVencedores(tabuleiro) == false) {
+    if (jogo.existeVencedor(tabuleiro) == false) {
         println("Houve um empate no jogo!")
         empate++
     }
@@ -217,11 +214,5 @@ fun main() {
 
 
 }
-
-
-
-
-
-
 
 
